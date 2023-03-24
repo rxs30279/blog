11b934af9a673ff46e2c82564be07b8ffd2d0eef
Richard <richard_stephens@hotmail.co.uk>
2023-03-24 13:42:56 +0000

    added tests

--- django_project/settings.py
+++ django_project/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.config()
+# DATABASES["default"] = dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
